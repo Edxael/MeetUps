86673714407f39caf20d97e30aef58633d2823e4
Edxael <zerphyr.r@gmail.com>
2018-04-17 19:03:32 -0600

    add the nots

--- 03-Promises/01-app/src/MyApp/00-Main.jsx
+++ 03-Promises/01-app/src/MyApp/00-Main.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import './style.css'
+let URL = 'https://swapi.co/api/P'
 
 export default class extends React.Component{
     render(){
 
-        const exe1 = () => {
+        const exe1 = async () => {
             console.log("Executing Promise: ")
 
+            const myans = await fetch(URL).then((resp) => { return resp.json() })
+                                          .then((resp) => { return resp })
+                                          .catch((err) => { console.log("Not in this UNIVERSE...") }) 
+
+            await console.log("My Answer: ", myans)
         }
         
         return(
             <div>
-                <h1>Code for Meet-Up</h1>
+                <h1>Code for Meet-Up.</h1>
                 <hr/>
 
                 <button onClick={ exe1 } >Use Promise</button>
